b0355e73111dc0134e58f009072d3537d727ad0d
ipolevoy <igor@expresspigeon.com>
2018-09-17 18:25:36 -0500

    upgraded to JL 2.2

--- src/main/java/app/config/AppControllerConfig.java
+++ src/main/java/app/config/AppControllerConfig.java
@@ -5,7 +5,6 @@ import app.controllers.PeopleController;
 import org.javalite.activeweb.AbstractControllerConfig;
 import org.javalite.activeweb.AppContext;
 import org.javalite.activeweb.controller_filters.DBConnectionFilter;
-import org.javalite.activeweb.controller_filters.TimingFilter;
 
 public class AppControllerConfig extends AbstractControllerConfig {
 
